d3ff11d80088b2075d95563303795fa60dcd75a0
Nikita Mikhaylov <nslqqq@gmail.com>
2012-12-30 23:12:01 +0700

    switch_monitor.sh works for both catalyst and radeon drivers

--- scripts/switch_monitor.sh
+++ scripts/switch_monitor.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-# Init numlockx
-internal=`xrandr | grep LVDS | sed -e "s/ .*//g"`
-external=`xrandr | grep DFP | sed -e "s/ .*//g"`
-has_external=$(xrandr | grep DFP | grep " connected" | wc -l)
-#echo "Internal: "$internal
-#echo "External: "$external
-#echo "External2: "$has_external
 
-if [[ $has_external -ne 0 ]]
+internal=`xrandr | grep LVDS | sed -e "s/ .*//g"`
+externals=('HDMI'   # xf86-video-radeon
+           'DFP')   # catalyst
+
+for name in "${externals[@]}"
+do
+    exist=$(xrandr | grep $name | grep " connected")
+
+    if [ -n "$exist" ]
+    then
+        external=`xrandr | grep $name | sed -e "s/ .*//g"`
+        break
+    fi
+done
+
+if [ -n "$external" ]
 then
-#    echo external mode
     xrandr --output $external --mode "1280x1024"
     xrandr --output $internal --off
 else
-#    echo internal mode
     xrandr --output $internal --mode "1366x768"
 fi
