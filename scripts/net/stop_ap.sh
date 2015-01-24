945a482e740424fe48c38030371513fb827c05a6
Nikita Mikhailov <nslqqq@gmail.com>
2015-01-24 12:18:40 +0600

    scripts: rearrange

---
+++ scripts/net/stop_ap.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+ip add flush dev wlan0
+#ip addr add 192.168.1.1/24 broadcast 192.168.1.255 dev wlan0
+/etc/rc.d/hostapd stop
+#iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
+#echo 1 > /proc/sys/net/ipv4/ip_forward
+rc.d stop dnsmasq
